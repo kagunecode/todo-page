1281d4cb82addd865b54b20ef362938af74c71c1
kagunecode <cesar.acb98@gmail.com>
2023-11-09 15:05:56 -0500

    Removed rendering, moved to elements instead

--- src/index.js
+++ src/index.js
@@ -3,10 +3,7 @@ import erasePage from "./functions/erasePage";
 import navbarLoad from "./pages/navbar";
 import loadHome from "./pages/home";
 import switchMode from "./functions/darkMode";
-import renderCircles from "./elements/bgCircles";
 
-renderCircles();
 loadHome();
 navbarLoad();
-
 switchMode();
