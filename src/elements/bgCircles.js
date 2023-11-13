52b38adac4a848ec5a939b0884397df9883f57cc
kagunecode <cesar.acb98@gmail.com>
2023-11-13 13:00:44 -0500

    Numeric input removed due to render errors

--- src/elements/bgCircles.js
+++ src/elements/bgCircles.js
@@ -1,28 +1,19 @@
-const renderCircles = (
-	firstposx,
-	firstposy,
-	secposx,
-	secposy,
-	thirdposx,
-	thirdposy
-) => {
-	const content = document.querySelector("#content");
-
+const renderCircles = () => {
 	const circlesContainer = document.createElement("div");
-	circlesContainer.className = "relative w-full max-w-lg";
+	circlesContainer.className = "absolute w-full h-full";
 
 	const fisrtCircle = document.createElement("div");
-	fisrtCircle.className = `absolute top-[${firstposx}rem] left-[${firstposy}rem] w-72 h-72 rounded-full bg-blue-300 dark:bg-black dark:blur-2xl mix-blend-multiply filter blur-xl opacity-90 animate-blob ease-in-out`;
+	fisrtCircle.className = `absolute top-[10rem] right-[45rem] w-72 h-72 rounded-full bg-blue-300 dark:bg-black dark:blur-2xl mix-blend-multiply filter blur-xl opacity-90 animate-blob ease-in-out`;
 
 	const secondCircle = document.createElement("div");
-	secondCircle.className = `absolute top-[${secposx}rem] -right-[${secposy}rem] w-72 h-72 rounded-full bg-blue-300 dark:bg-black dark:blur-2xl mix-blend-multiply blur-xl opacity-50 animate-blob animation-delay-2000 ease-in-out`;
+	secondCircle.className = `absolute top-[10rem] right-[60rem] w-72 h-72 rounded-full bg-blue-300 dark:bg-black dark:blur-2xl mix-blend-multiply blur-xl opacity-50 animate-blob animation-delay-2000 ease-in-out`;
 
 	const thirdCircle = document.createElement("div");
-	thirdCircle.className = `absolute -bottom-[${thirdposx}rem] left-[${thirdposy}rem] w-72 h-72 rounded-full bg-blue-300 dark:bg-black dark:blur-2xl mix-blend-multiply blur-xl opacity-50 animate-blob animation-delay-4000 ease-in-out`;
+	thirdCircle.className = `absolute bottom-[10rem] right-[50rem] w-72 h-72 rounded-full bg-blue-300 dark:bg-black dark:blur-2xl mix-blend-multiply blur-xl opacity-50 animate-blob animation-delay-4000 ease-in-out`;
 
-	circlesContainer.appendChild(fisrtCircle);
-	circlesContainer.appendChild(secondCircle);
 	circlesContainer.appendChild(thirdCircle);
+	circlesContainer.appendChild(secondCircle);
+	circlesContainer.appendChild(fisrtCircle);
 
 	return circlesContainer;
 };
