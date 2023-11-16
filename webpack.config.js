1a97ddf2a8a38804d870fd9a55fc2476961ce93e
kagunecode <cesar.acb98@gmail.com>
2023-11-16 13:03:54 -0500

    Added importloader for css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,11 @@ module.exports = {
 			{
 				test: /\.css$/i,
 				include: path.resolve(__dirname, "src"),
-				use: ["style-loader", "css-loader", "postcss-loader"],
+				use: [
+					"style-loader",
+					{ loader: "css-loader", options: { importLoaders: 1 } },
+					"postcss-loader",
+				],
 			},
 			{
 				test: /\.svg$/,
