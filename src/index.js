3ede4d5951c80343b3be960cbeef4d49ac51c510
kagunecode <cesar.acb98@gmail.com>
2023-11-18 13:26:02 -0500

    Added modal window for login

--- src/index.js
+++ src/index.js
@@ -11,3 +11,18 @@ switchMode();
 document.getElementById("start-button").addEventListener("click", () => {
 	window.location.href = "app.html";
 });
+
+const modal = document.querySelector("dialog");
+const closeModal = document.querySelector("#modal-close");
+const buttonUser = document.querySelector("#button-log");
+const htmlBody = document.querySelector("html");
+
+closeModal.addEventListener("click", () => {
+	modal.close();
+	//htmlBody.classList.remove("overflow-hidden");
+});
+
+buttonUser.addEventListener("click", () => {
+	modal.showModal();
+	//htmlBody.classList.add("overflow-hidden");
+});
