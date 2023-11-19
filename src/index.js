bf8aaa1706443b4bb237727dd2c5f6722f6bcf43
kagunecode <cesar.acb98@gmail.com>
2023-11-18 20:41:45 -0500

    Moved functions into modules

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import erasePage from "./functions/erasePage";
 import navbarLoad from "./pages/navbar";
 import loadHome from "./pages/home";
 import switchMode from "./functions/darkMode";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { login } from "./functions/userAuth";
+import { checkEmail, checkPassword } from "./functions/strVerification";
 
 loadHome();
 navbarLoad();
@@ -15,14 +20,36 @@ document.getElementById("start-button").addEventListener("click", () => {
 const modal = document.querySelector("dialog");
 const closeModal = document.querySelector("#modal-close");
 const buttonUser = document.querySelector("#button-log");
-const htmlBody = document.querySelector("html");
+const loginButton = document.querySelector("#login-button");
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyCiLNpG3XgkGWcpLtN2DzyJraeJ-riM-aA",
+	authDomain: "todo-app-adf75.firebaseapp.com",
+	projectId: "todo-app-adf75",
+	storageBucket: "todo-app-adf75.appspot.com",
+	messagingSenderId: "484620164961",
+	appId: "1:484620164961:web:ce40d8221e06a923432f20",
+};
 
 closeModal.addEventListener("click", () => {
 	modal.close();
-	//htmlBody.classList.remove("overflow-hidden");
 });
 
 buttonUser.addEventListener("click", () => {
 	modal.showModal();
-	//htmlBody.classList.add("overflow-hidden");
 });
+
+loginButton.addEventListener("click", () => {
+	let email = document.querySelector("#input-email").value;
+	let password = document.querySelector("#input-password").value;
+	if (checkEmail(email) && checkPassword(password)) {
+		login(auth, email, password);
+	}
+});
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// pass es lacobra11
