3fb3def2f7e074ef55abc63ff684f6d813747d3e
kagunecode <cesar.acb98@gmail.com>
2023-11-19 15:26:41 -0500

    Added correct script and logout function

--- src/pages/app.js
+++ src/pages/app.js
@@ -1,10 +1,16 @@
-//import "../styles.css";
+import "../styles.css";
+import { logOut } from "../functions/userAuth";
 
 const menuButton = document.querySelector("#menu-button");
 const contentWindow = document.querySelector("#screen-space");
+const logoutButtton = document.querySelector("#test-button");
 
 menuButton.addEventListener("click", () => {
 	const sidebar = document.querySelector("#sidebar");
 	sidebar.classList.toggle("-translate-x-[100%]");
 	contentWindow.classList.toggle("-ml-[21rem]");
+});
+
+logoutButtton.addEventListener("click", () => {
+	logOut();
 });
