3bbb3bd43c12c0318481609f41dd2949cb0ad309
kagunecode <cesar.acb98@gmail.com>
2023-11-21 12:34:34 -0500

    Configured dotenv to work with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
 	mode: "development",
@@ -81,5 +82,6 @@ module.exports = {
 			template: "src/views/app.html",
 			chunks: ["app"],
 		}),
+		new Dotenv(),
 	],
 };
